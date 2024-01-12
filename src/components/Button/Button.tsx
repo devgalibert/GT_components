c794272c8338fd7fec5ae97d46be92338d84c6eb
Thomas GALIBERT <thomas.galibert@humansconnexion.com>
2024-01-12 16:32:22 +0100

    Update Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { MouseEventHandler } from "react";
 
 export interface ButtonProps {
   label: string;
+  onClick: MouseEventHandler;
+  className: string;
+  disabled: boolean;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props?.label ?? "default button"}</button>;
+  return (
+    <button
+      disabled={props.disabled}
+      className={props.className}
+      onClick={props.onClick}
+    >
+      {props?.label ?? "default button"}
+    </button>
+  );
 };
 
 export default Button;
